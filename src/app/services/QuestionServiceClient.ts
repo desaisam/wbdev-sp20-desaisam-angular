3e53c59e05f7bb4cac04f64f6450b211f0227c57
Sameer <desai.sam@husky.neu.edu>
2020-04-22 11:32:55 -0400

    heroku

--- src/app/services/QuestionServiceClient.ts
+++ src/app/services/QuestionServiceClient.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class QuestionServiceClient {
     findQuestionsForQuiz = (qid) =>
-        fetch(`http://https://wbdev-sp20-desaisam-node.herokuapp.com/api/quizzes/${qid}/questions`)
+        fetch(`https://https://wbdev-sp20-desaisam-node.herokuapp.com/api/quizzes/${qid}/questions`)
             .then(response => response.json())
 }
--- src/app/services/QuizServiceClient.ts
+++ src/app/services/QuizServiceClient.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
-    fetch('http://wbdev-sp20-desaisam-node.herokuapp.com/api/quizzes')
+    fetch('https://wbdev-sp20-desaisam-node.herokuapp.com/api/quizzes')
       .then(response => response.json())
   findQuizById = (qid) =>
     fetch(`https://wbdev-sp20-desaisam-node.herokuapp.com/api/quizzes/${qid}`)
