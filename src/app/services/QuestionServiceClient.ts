3bce01ef8ff005fcb9880750fe233a29052a6e3e
Sameer <desai.sam@husky.neu.edu>
2020-04-22 13:11:05 -0400

    heroku

--- src/app/services/QuestionServiceClient.ts
+++ src/app/services/QuestionServiceClient.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class QuestionServiceClient {
     findQuestionsForQuiz = (qid) =>
-        fetch(`https://https://wbdev-sp20-desaisam-node.herokuapp.com/api/quizzes/${qid}/questions`)
+        fetch(`https://wbdev-sp20-desaisam-node.herokuapp.com/api/quizzes/${qid}/questions`)
             .then(response => response.json())
 }
